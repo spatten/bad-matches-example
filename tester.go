8cf6326dcd86562036d2941f6753528d19933fd0
spatten <scott@fossa.com>
2022-11-16 17:38:02 -0800

    add some tester files

---
+++ tester.go
@@ -0,0 +1,24 @@
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+//go:build icu
+// +build icu
+
+// Copyright 2016 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Copyright 2016 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+package cases
+
+import (
+	"testing"
+)
+
+func TestMITConformance(t *testing.T) {
+}
